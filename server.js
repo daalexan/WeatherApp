cccf1a0ed0455f9fe33133965341bb0c80e536cd
daalexan <daniel.alexandrov.s@gmail.com>
2020-09-18 01:05:29 +0300

    deploy config for heroku

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/WeatherApp'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/WeatherApp'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/WeatherApp/'}),
-);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/WeatherApp/index.html'));
+});
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
